abdea02d45c64506f23d4fae162bce97b31fa627
ftopu <43989411+ftopu@users.noreply.github.com>
2020-10-08 10:30:37 +0100

    Add files via upload

---
+++ Quizz/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quizzz
+{
+    class Player
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public Score Score { get; set; }
+
+        public Player(string id, string name, Score score) => 
+            (Id, Name, Score) = (id, name, score);
+
+        
+
+        
+    }
+}
---
+++ Quizz/Question.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quizzz
+{
+    class Question
+    {
+        public string MainQuestion { get; set; }
+        public List<String> Answers { get; set; }
+        public List<String> CorrectAnswers { get; set; }
+        public double Difficulty { get; set; }
+        public string Category { get; set; }
+        public int NumberOfAnswers { get; set; }
+        public int NumberOfCorrectAnswers { get; set; }
+
+
+        // Constructor
+        public Question(string mainQuestion, List<String> answers, List<String> correctAnswers, double difficulty, string category, int numberOfAnswers, int numberOfCorrectAnswers) =>
+            (MainQuestion, Answers, CorrectAnswers, Difficulty, Category, NumberOfAnswers, NumberOfCorrectAnswers) = 
+            (mainQuestion, answers, correctAnswers, difficulty, category, numberOfAnswers, numberOfCorrectAnswers);
+
+    }
+}
