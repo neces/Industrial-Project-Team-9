efbd25029008ac80626eafbdbdd463426c123b44
Neja's Mac <neja.cesnik@hotmail.com>
2020-10-08 15:31:04 +0100

    First app project

---
+++ Software-Code/Quiz/Score.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quiz
+{
+    class Score
+    {
+        public int CurrentScore { get; private set; }
+
+        public Score(int currentScore) => 
+            CurrentScore = currentScore;
+
+        
+
+
+        public void updateScore(int update) => 
+            CurrentScore += update;
+
+        
+    }
+}
