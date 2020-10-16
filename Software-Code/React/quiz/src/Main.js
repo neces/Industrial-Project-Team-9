f799970a2e93630a9bee089c87003d353385f677
Neja's Mac <neja.cesnik@hotmail.com>
2020-10-16 23:04:41 +0100

    Functional homepage and input form

--- Software-Code/React/quiz/src/Main.js
+++ Software-Code/React/quiz/src/Main.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
 import Home from './pages/Home';
 import Host from './pages/Host';
 import Quiz from './pages/Quiz';
 
 const Main = () => {
+
   return (
     <Switch>
       <Route exact path='/' component={Home}></Route>
--- Software-Code/React/quiz/src/components/Timer.js
+++ Software-Code/React/quiz/src/components/Timer.js
@@ -1,8 +1,35 @@
 import React from "react";
 
-const Timer = ({}) => {
+const Timer = ({timer}) => 
+{
+  let [counter, setCounter] = React.useState(timer);
 
+  React.useEffect(() => 
+  {
+    counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
+  }, [counter]);
 
+  const showAnswer = () => {
+
+  }
+
+  if(counter === 0)
+  {
+      return (
+        <div className="App">
+        <div></div>
+        </div>
+      )
+  }
+  else
+  {
+    return (
+        <div className="App">
+        <div>Time Left: {counter}</div>
+        </div>
+      )
+  }
+  
 }
 
 export default Timer;
