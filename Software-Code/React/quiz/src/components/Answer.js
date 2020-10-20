f200dea02b57b6325d1cf578929b19ef02cf7808
Neja's Mac <neja.cesnik@hotmail.com>
2020-10-20 19:20:38 +0100

    Host page now includes Quiz ID, answering now working again

--- Software-Code/React/quiz/src/components/Answer.js
+++ Software-Code/React/quiz/src/components/Answer.js
@@ -1,11 +1,9 @@
-import React, { useState, createRef, useRef } from "react"
+import React, { useState } from "react"
 import axios from 'axios'
 
 const Answer = ({ answers, type, userID }) => {
     const [isAnswerRight, setIsAnswerRight] = useState(false)
     const [answered, setAnswered] = useState(false)
-    const buttonRef = createRef();
-
 
     const sendAnswer = (answer) => {
       axios
@@ -20,40 +18,17 @@ const Answer = ({ answers, type, userID }) => {
       .catch(error => {
         console.error('There was an error!', error);
       })
-    } // what is this was sending back the correct answer?
-
-    const showAnswer = () => {
-      // color the correct answer
-      // after the timer is done
     }
-
-    const disableButton = () => {
-      buttonRef.current.disabled = true; // this disables the button
-     }
     
     const handleAnswerOptionClick = (answer) => {
-      // until a new question is called
-        if (answered === false) {
-          sendAnswer(answer)
-          setAnswered(true)
-        }
-        //disableButton()
-        // return (
-        //   <div className='answer-section'>
-        //     {questions.answers.map((answer) => (
-        //     <button disabled key={answer}>{answer}</button>
-        //     ))}
-        //   </div>
-        // )
-        // disable all buttons
-        // colour the chosen button select buttons whos value is answer and change the color to pink
+        sendAnswer(answer)
     }
 
   if (type === "MultiChoice") {
       return (
         <div className='answer-section'>
           {answers.map((answer) => (
-          <button ref={buttonRef} onClick={() => handleAnswerOptionClick(answer)} key={answer}>{answer}</button>
+          <button onClick={() => handleAnswerOptionClick(answer)} key={answer}>{answer}</button>
           ))}
         </div>
       )
@@ -63,7 +38,7 @@ const Answer = ({ answers, type, userID }) => {
     return (
       <div className='answer-section'>
           {answers.map((answer) => (
-          <button ref={buttonRef} onClick={() => handleAnswerOptionClick(answer)} key={answer}>{answer}</button>
+          <button onClick={() => handleAnswerOptionClick(answer)} key={answer}>{answer}</button>
           ))}
       </div>
     )
