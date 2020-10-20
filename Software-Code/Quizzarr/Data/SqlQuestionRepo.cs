dc123f54e9c7dbf74957d86703abf986c7e1c27a
Cameron McNeill <cameron.mcneill24@hotmail.co.uk>
2020-10-20 17:33:37 +0100

    Connection and question retrieval complete

--- Software-Code/Quizzarr/Data/SqlQuestionRepo.cs
+++ Software-Code/Quizzarr/Data/SqlQuestionRepo.cs
@@ -38,7 +38,9 @@ namespace Quizzarr.Data
             List<Question> questions = new List<Question>();
             objConn.Open();
 
-            SqlDataAdapter daQuestions = new SqlDataAdapter("Select * from QuestionsTable", objConn);
+            string mainQuery = "SELECT TOP " + count + " * FROM QuestionsTable WHERE Type='MultiChoice' OR Type='TrueFalse' ORDER BY NEWID()";
+
+            SqlDataAdapter daQuestions = new SqlDataAdapter(mainQuery, objConn);
 
             DataSet dsQuestions = new DataSet(_DBName);
 
@@ -48,43 +50,44 @@ namespace Quizzarr.Data
 
             Console.WriteLine(tblQuestions.Rows.Count);
 
+            int index = 0;
+
             foreach (DataRow dr in tblQuestions.Rows) {
+                try {
 
-                // Check type of qustion
-                // create alt answers list
-                // if question is multichoice
-                //      populate list with alt answers
-                // set altAnsers to list
-
-                List<string> __altAnswers = new List<string>();
-                if (dr["Type"].ToString().Equals("MultiChoice")) {
-                    string query = "Select * from MultiChoiceTable where ID='" + dr["MultiChoice_ID"].ToString() + "'";
-                    SqlDataAdapter daAnswers = new SqlDataAdapter(query, objConn);
-                    DataSet dsAnswers = new DataSet(_DBName);
-                    daAnswers.Fill(dsAnswers, _MultiChoice);
-                    DataTable tblAnswers = dsAnswers.Tables[_MultiChoice];
-                    
-                    // FIX THIS BIT HERE PLEASE CAUSE WHOOPIES IT DOESNT WORK
-                    if (tblAnswers.Rows[0]) {
+                    List<string> __altAnswers = new List<string>();
+                    if (dr["Type"].ToString().Equals("MultiChoice")) {
+                        string query = "Select * from MultiChoiceTable where ID='" + dr["MultiChoice_ID"].ToString() + "'";
+                        SqlDataAdapter daAnswers = new SqlDataAdapter(query, objConn);
+                        DataSet dsAnswers = new DataSet(_DBName);
+                        daAnswers.Fill(dsAnswers, _MultiChoice);
+                        DataTable tblAnswers = dsAnswers.Tables[_MultiChoice];
+                        
                         DataRow drA = tblAnswers.Rows[0];
                         __altAnswers.Add(drA["AltAnswer01"].ToString());
                         __altAnswers.Add(drA["AltAnswer02"].ToString());
                         __altAnswers.Add(drA["AltAnswer03"].ToString());
                     }
+
+                    int _id = int.Parse(dr["ID"].ToString());
+                    string _type = dr["Type"].ToString();
+                    string _question = dr["Question"].ToString();
+                    string _answer = dr["Answer"].ToString();
+                    string _topic = dr["Topic"].ToString();
+
+                    string _multiChoice_ID_string = (dr["MultiChoice_ID"].ToString().Equals("")) ? "-1" : dr["MultiChoice_ID"].ToString();
+                    int _multiChoice_ID = int.Parse(_multiChoice_ID_string);
+
+                    double _numCorrect = Double.Parse(dr["NumCorrect"].ToString());
+                    double _numIncorrect = Double.Parse(dr["NumIncorrect"].ToString());
+                    double _difficulty = Double.Parse(dr["Difficulty"].ToString());
+                    List<string> _altAnswers = __altAnswers;
+
+                    questions.Add( new Question(_id, _type, _question, _answer, _topic, _multiChoice_ID, _numCorrect, _numIncorrect, _difficulty, _altAnswers));
+                } catch (Exception e) {
+                    System.Console.WriteLine("Error Occured with index - " + index);
                 }
-
-                int _id = int.Parse(dr["ID"].ToString());
-                string _type = dr["Type"].ToString();
-                string _question = dr["Question"].ToString();
-                string _answer = dr["Answer"].ToString();
-                string _topic = dr["Topic"].ToString();
-                int _multiChoice_ID = (dr["MultiChoice_ID"].Equals("")) ? int.Parse(dr["MultiChoice_ID"].ToString().Trim()) : -1;
-                double _numCorrect = Double.Parse(dr["NumCorrect"].ToString());
-                double _numIncorrect = Double.Parse(dr["NumIncorrect"].ToString());
-                double _difficulty = Double.Parse(dr["Difficulty"].ToString());
-                List<string> _altAnswers = __altAnswers;
-
-                questions.Add( new Question(_id, _type, _question, _answer, _topic, _multiChoice_ID, _numCorrect, _numIncorrect, _difficulty, _altAnswers));
+                index++;
             }
             objConn.Close();
 
