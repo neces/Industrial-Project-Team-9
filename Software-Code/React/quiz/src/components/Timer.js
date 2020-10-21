211f1ef43f77628d57e90060397e28400aa140d3
Neja's Mac <neja.cesnik@hotmail.com>
2020-10-21 11:05:05 +0100

    Integrating Timer and adding Leave component

--- Software-Code/React/quiz/src/components/Timer.js
+++ Software-Code/React/quiz/src/components/Timer.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react'
 
-const Timer = ({ timer, correctAnswer, handleFilterAnswer }) => {
+const Timer = ({ timer, handleFilterAnswer }) => {
   const [counter, setCounter] = useState(timer)
   const [isTimeout, setIsTimeout] = useState(false)
 
-  useEffect(() => 
-  {
+  useEffect(() => {
     counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
   }, [counter]);
 
@@ -16,11 +15,11 @@ const Timer = ({ timer, correctAnswer, handleFilterAnswer }) => {
       setTimeout(() => {
         setCounter(timer)
         setIsTimeout(false)
-      }, 5000);
+      }, 5000); // this is the time left for showing the correct answer at the end, could change it to 3000
       setIsTimeout(true)
     }
     return (
-      <div className="App">
+      <div className="timer">
       <div></div>
       </div>
     )
@@ -28,8 +27,8 @@ const Timer = ({ timer, correctAnswer, handleFilterAnswer }) => {
   else
   {
     return (
-        <div className="App">
-        <div>Time Left: {counter}</div>
+        <div className="timer">
+        <div>{counter}</div>
         </div>
       )
   }
