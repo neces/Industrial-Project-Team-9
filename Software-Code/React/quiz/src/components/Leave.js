0b5b6a38610bc4f3d538955704cf26e86a3ab2a4
awbrodrick <46675102+awbrodrick@users.noreply.github.com>
2020-10-22 18:21:03 +0100

    Added Leave Session Functionality
    Added an automatic leave session function into the leaderboard, which will also end the session when the host leaves the game.

--- Software-Code/React/quiz/src/components/Leave.js
+++ Software-Code/React/quiz/src/components/Leave.js
@@ -2,7 +2,7 @@ import React, {useState} from "react"
 import axios from 'axios'
 import { Redirect } from 'react-router-dom'
 
-const Leave = ({ userID }) => {
+const Leave = ({ userID, isLeaderboard }) => {
     const [left, setLeft] = useState(false)
 
     const leaveSession = (event) => {
@@ -20,17 +20,60 @@ const Leave = ({ userID }) => {
         })
       }
 
+      const leaveSessionAuto = () => {
+        axios
+        .get('https://team9app.azurewebsites.net/api/quizzarr/leaveSession', { params: {
+          userID,
+        }})
+        .then(response => {
+          console.log(response.data);
+          checkIfHost()
+          setLeft(true)
+        })
+        .catch(error => {
+          console.error('There was an error!', error);
+        })
+      }
+
+      const checkIfHost = () => {
+        axios
+        .call('https://team9app.azurewebsites.net/api/quizzarr/admin/endSession', { params: {
+         userID,
+        }})
+        .then(response => {
+          console.log(response.data);
+          setLeft(true)
+          console.log('Session Deleted')
+        })
+        .catch(error => {
+          console.error('There was an error!', error);
+        })
+
+      }
+
     if (left === true) {
         return (
             <Redirect to="/"/>
         )
     }
 
-    return (
+    if(isLeaderboard === true)
+    {
+      setTimeout(() => {
+        leaveSessionAuto()
+      }, 10000);
+      return(
+        <div>Session will end in 10 seconds</div>
+      )
+    }
+    else
+    {
+      return (
         <div>
         <button className='button-leave' onClick={leaveSession}>LEAVE SESSION</button>
         </div>
-    )
+            )
+    }
 }
 
   export default Leave;
