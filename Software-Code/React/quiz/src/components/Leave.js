dee1cd54f9d79768cabd7b39c16355953533a1f8
awbrodrick <46675102+awbrodrick@users.noreply.github.com>
2020-10-25 13:50:46 +0000

    Added comments To Pages
    Added comments to the Leave.js and Timer.js file

--- Software-Code/React/quiz/src/components/Leave.js
+++ Software-Code/React/quiz/src/components/Leave.js
@@ -5,8 +5,15 @@ import { Redirect } from 'react-router-dom'
 const Leave = ({ userID, isLeaderboard }) => {
     const [left, setLeft] = useState(false)
 
+    //Function which allows the user to leave the session successfully
+    //takes in an event parameter which is populated when the user clicks 
     const leaveSession = (event) => {
         event.preventDefault();
+
+        //Checks to see if the current page is the leaderboard if not
+        //The api will be called to get the ability for the user to leave the sesssion
+        //if successful the user will have left the session
+        //if unsuccessful an error will be returned to the console
         if(isLeaderboard === false)
         {
         axios
@@ -21,10 +28,17 @@ const Leave = ({ userID, isLeaderboard }) => {
         })
       }
 
-        setLeft(true)
+      //this will set the global variable left to true
+      setLeft(true)
       }
 
+      //Function which is similar to leaveSession however this will be automatically run if isLeaderboard is true
       const leaveSessionAuto = () => {
+
+        //Checks to see if the current page is the leaderboard if not
+        //The api will be called to get the ability for the user to leave the sesssion
+        //if successful the user will have left the session
+        //if unsuccessful an error will be returned to the console
         axios
         .get('https://team9app.azurewebsites.net/api/quizzarr/leaveSession', { params: {
           userID,
@@ -38,7 +52,13 @@ const Leave = ({ userID, isLeaderboard }) => {
         })
       }
 
+      //Function to check if the user leaving is the host and will end the session if so
+      //will only be called on the leaderboard page
       const checkIfHost = () => {
+
+        //Checks to see if the current user is the host
+        //if so the session will end
+        //if not then the session will not end
         axios
         .call('https://team9app.azurewebsites.net/api/quizzarr/admin/endSession', { params: {
          userID,
@@ -53,16 +73,20 @@ const Leave = ({ userID, isLeaderboard }) => {
 
       }
 
+      //if the variable left is true
     if (left === true) {
         return (
+          //redirect the user to the homepage of the app
             <Redirect to="/"/>
         )
     }
 
       
-
+      //checks to see if the isLeaderboard variable passed through is true
       if(isLeaderboard === true)
       {
+        //if so the session will be automatically left through the leaveSessionAuto function
+        //A button will be displayed on the screen allowing the user to return to the homepage if they wish
         leaveSessionAuto()
 
         return (
@@ -74,6 +98,7 @@ const Leave = ({ userID, isLeaderboard }) => {
       }
       else
       {
+        //if not a button will be displayed on the screen allowing the user to return to the homepage if they wish which will also leave the session if the user is not on the leaderboard page
         return (
         
           <div>
--- Software-Code/React/quiz/src/components/Timer.js
+++ Software-Code/React/quiz/src/components/Timer.js
@@ -4,21 +4,33 @@ const Timer = ({ timer, handleIsTimeOut, resetTimeIsOut }) => {
   const [counter, setCounter] = useState(timer)
   const [isTimeout, setIsTimeout] = useState(false)
 
+  //An effect is applied to the counter variable where every second the number will be reduced by one and then returned
   useEffect(() => {
     counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
   }, [counter]);
 
+  //if the counter reaches 0 then
   if (counter === 0)
   {
+    //check if the variable isTimeout is false then
     if (isTimeout === false) {
+      //call function handleIsTimeOut from the Quiz.js page
+      //then set the variable isTimeout while
       handleIsTimeOut()
       setTimeout(() => {
+        // call function resetTimeIsOut from the Quiz.js page
+        //set the counter variable to the value of timer from the variable passed through
+        //set the IsTimeout variable to false
         resetTimeIsOut()
         setCounter(timer)
         setIsTimeout(false)
       }, 2000); // this is the time left for showing the correct answer at the end
+
+      //then set the IsTimeout variable to true
       setIsTimeout(true)
     }
+
+    //return nothing to the page call
     return (
       <div className="timer">
       <div>&nbsp;</div>
@@ -27,6 +39,8 @@ const Timer = ({ timer, handleIsTimeOut, resetTimeIsOut }) => {
   }
   else
   {
+    //if the counter does not reach 0 then
+    //return the counter variable to the page call
     return (
         <div className="timer">
         <div>{counter}</div>
