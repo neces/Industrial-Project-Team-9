3824c0ede014ec3161bc8e0031fcde77011b48de
Neja's Mac <neja.cesnik@hotmail.com>
2020-10-25 11:57:12 +0000

    Accessibility improvements in the design and code

--- Software-Code/React/quiz/src/pages/Leaderboard.js
+++ Software-Code/React/quiz/src/pages/Leaderboard.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import Cookies from 'universal-cookie'
 import Leave from '../components/Leave'
 
-const Leaderboard = ({ }) => {
+const Leaderboard = () => {
     const [leaderboard, setLeaderboard] = useState([])
     const [isLoadingLeaderboard, setLoadingLeaderboard] = useState(true)
     const cookies = new Cookies()
@@ -35,7 +35,7 @@ const Leaderboard = ({ }) => {
                     <img src={require('../assets/1stPlace.png')} alt='First place' width="300"></img>
                     <div>{leaderboard[0].displayName} / Score <b>{leaderboard[0].score}</b> / Highest Streak <b>{leaderboard[0].highestStreak}</b></div>
                 </div>
-                {/* <Leave userID={cookies.get('userID')} isLeaderboard={true} /> */}
+                <Leave userID={cookies.get('userID')} isLeaderboard={true} />
             </div>
         )
     }
@@ -51,7 +51,7 @@ const Leaderboard = ({ }) => {
                      <img src={require('../assets/2ndPlace.png')} alt='Second place' width="300"></img>
                      <div>{leaderboard[1].displayName} / Score <b>{leaderboard[1].score}</b> / Highest Streak <b>{leaderboard[1].highestStreak}</b></div>
                  </div>
-                {/* <Leave userID={cookies.get('userID')} isLeaderboard={true} /> */}
+                <Leave userID={cookies.get('userID')} isLeaderboard={true} />
             </div>
         )
     }
@@ -71,7 +71,7 @@ const Leaderboard = ({ }) => {
                     <img src={require('../assets/3rdPlace.png')} alt='Third place' width="300"></img>
                     <div>{leaderboard[2].displayName} / Score <b>{leaderboard[2].score}</b> / Highest Streak <b>{leaderboard[2].highestStreak}</b></div>
                 </div>
-                {/* <Leave userID={cookies.get('userID')} isLeaderboard={true} /> */}
+                <Leave userID={cookies.get('userID')} isLeaderboard={true} />
             </div>
         )
     }
@@ -95,10 +95,10 @@ const Leaderboard = ({ }) => {
                     <img src={require('../assets/Leaderboard.png')} alt='Leaderboard' width="1000"></img>
                     <div className='leaderboard-text'>{leaderboard.map((user, i) => {return (<div key={i+1} className="rank"> <b>{i+1}.</b> {user.displayName} / Score <b>{user.score}</b> / Highest Streak <b>{user.highestStreak}</b></div>)})}</div>
                 </div>
-                {/* <Leave userID={cookies.get('userID')} isLeaderboard={true} /> */}
+                <Leave userID={cookies.get('userID')} isLeaderboard={true} />
             </div>
         )
     }
 }
 
-export default Leaderboard;
+export default Leaderboard
